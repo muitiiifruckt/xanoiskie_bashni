fcacdd9d13ab17b308ba14a658be032bd1428caa
multifrukt <multifrukt@MULTIFRUKT>
2023-01-07 13:53:39 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Ханойские_башни
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Speed = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button_solve = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pic_hotisontal_tower = new System.Windows.Forms.PictureBox();
+            this.pic_tower_3 = new System.Windows.Forms.PictureBox();
+            this.pic_tower_2 = new System.Windows.Forms.PictureBox();
+            this.pic_tower_1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_hotisontal_tower)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tower_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tower_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tower_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.Speed);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button_solve);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(1501, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 753);
+            this.panel1.TabIndex = 0;
+            // 
+            // Speed
+            // 
+            this.Speed.AutoSize = true;
+            this.Speed.Location = new System.Drawing.Point(12, 62);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(48, 16);
+            this.Speed.TabIndex = 2;
+            this.Speed.Text = "Speed";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Count of Disks";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(5, 81);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(115, 22);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(5, 29);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(115, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button_solve
+            // 
+            this.button_solve.Location = new System.Drawing.Point(5, 153);
+            this.button_solve.Name = "button_solve";
+            this.button_solve.Size = new System.Drawing.Size(115, 23);
+            this.button_solve.TabIndex = 0;
+            this.button_solve.Text = "Solve";
+            this.button_solve.UseVisualStyleBackColor = true;
+            this.button_solve.Click += new System.EventHandler(this.button_solve_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(1086, 733);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(180, 20);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(601, 733);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(180, 20);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pic_hotisontal_tower
+            // 
+            this.pic_hotisontal_tower.BackColor = System.Drawing.Color.Black;
+            this.pic_hotisontal_tower.Location = new System.Drawing.Point(146, 733);
+            this.pic_hotisontal_tower.Name = "pic_hotisontal_tower";
+            this.pic_hotisontal_tower.Size = new System.Drawing.Size(180, 20);
+            this.pic_hotisontal_tower.TabIndex = 1;
+            this.pic_hotisontal_tower.TabStop = false;
+            // 
+            // pic_tower_3
+            // 
+            this.pic_tower_3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pic_tower_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic_tower_3.Location = new System.Drawing.Point(1169, 253);
+            this.pic_tower_3.Name = "pic_tower_3";
+            this.pic_tower_3.Size = new System.Drawing.Size(15, 500);
+            this.pic_tower_3.TabIndex = 2;
+            this.pic_tower_3.TabStop = false;
+            // 
+            // pic_tower_2
+            // 
+            this.pic_tower_2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pic_tower_2.Location = new System.Drawing.Point(685, 253);
+            this.pic_tower_2.Name = "pic_tower_2";
+            this.pic_tower_2.Size = new System.Drawing.Size(15, 500);
+            this.pic_tower_2.TabIndex = 2;
+            this.pic_tower_2.TabStop = false;
+            // 
+            // pic_tower_1
+            // 
+            this.pic_tower_1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pic_tower_1.Location = new System.Drawing.Point(232, 253);
+            this.pic_tower_1.Name = "pic_tower_1";
+            this.pic_tower_1.Size = new System.Drawing.Size(15, 500);
+            this.pic_tower_1.TabIndex = 2;
+            this.pic_tower_1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(1633, 753);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pic_hotisontal_tower);
+            this.Controls.Add(this.pic_tower_3);
+            this.Controls.Add(this.pic_tower_2);
+            this.Controls.Add(this.pic_tower_1);
+            this.Name = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_hotisontal_tower)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tower_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tower_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_tower_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_solve;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pic_hotisontal_tower;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pic_tower_1;
+        private System.Windows.Forms.PictureBox pic_tower_2;
+        private System.Windows.Forms.PictureBox pic_tower_3;
+        private System.Windows.Forms.Label Speed;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ханойские_башни.Custom.CodeeloUI.Controls;
+
+namespace Ханойские_башни
+{
+
+    public partial class Form1 : Form
+    {
+        // стэк для дальнейшего удаления созданных дисков
+        Stack<PictureBox> tower_1_picturebox = new Stack<PictureBox>(); 
+        Stack<PictureBox> tower_2_picturebox = new Stack<PictureBox>();
+        Stack<PictureBox> tower_3_picturebox = new Stack<PictureBox>();
+        // стэк для дальнейшего удаления созданных дисков
+
+        // координаты рисунков для дальнейшего реализации анимации
+        int x1, y1, x2, y2,s_x,s_y,s_y2,speed, type_of_speed ,time;
+        // координаты рисунков для дальнейшего реализации анимации
+
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = 3;// при открытие сразу будет 3 диска 
+
+        }
+
+        // проверка можем ли мы добавить из одной башни во вторую диск
+        private bool can_add(Stack <PictureBox> from,Stack <PictureBox> to)
+        {
+            if(from.Count == 0 ) return false;// из пустого нельзя перекинуть
+            if(to.Count == 0 ) return true;// в пустой всегда можно перекинуть
+            if(from.Peek().Width >to.Peek().Width ) return false;// больший диск на меньший нельзя перекинуть
+            return true;  // в остальных случаях можно перекинуть
+        }
+
+
+        // функция перемещения дисков
+        private void move(Stack<PictureBox> from, Stack<PictureBox> to,PictureBox fist_tower, PictureBox second_tower)
+        {
+
+            int n = Decimal.ToInt32(numericUpDown1.Value);
+            // а тут плавное перемещение графическое
+
+            int y = pic_tower_1.Location.Y; // верхняя точка башни (значение по игрику)
+
+            x1 = fist_tower.Location.X;
+            x2 = second_tower.Location.X;
+
+
+
+
+            // Перемещения которые должны произойти по Х и У
+
+            s_x = x2 - x1;
+
+            s_y = from.Peek().Location.Y - y  + from.Peek().Height; // сначала вверх
+
+            if(to.Count==0) // отдельно отработаь слуачай когда башня, в который добавляем пуста
+            {
+                s_y2 = pic_hotisontal_tower.Location.Y - y;
+            }
+            else
+                s_y2 = to.Peek().Location.Y - y;// потом вниз
+
+                
+            transfer(from, to); // перемещаем на форме
+
+            to.Push(from.Pop());// перемещаем в стеках
+
+        }
+
+        // перемещение дисков на форме
+        private void transfer(Stack<PictureBox> from, Stack<PictureBox> to)
+        {
+
+            // получаем значение типа скорости из нижнего нумерика
+            type_of_speed = Decimal.ToInt32(numericUpDown2.Value);
+            switch (type_of_speed) // 5 скоростей 
+            {
+                case 1:
+                    time = 1;
+                    speed = 1;
+                    break;
+                case 2:
+                    time = 1;
+                    speed = 2;
+                    break;
+                case 3:
+                    time = 0;
+                    speed = 1;
+                    break;
+                case 4:
+                    time = 0;   
+                    speed = 2;
+                    break;
+                case 5:
+                    time = 0;   
+                    speed = 20;
+                    break;
+            }
+
+            // если скорость от 3 и более вертикальная анимация будет представлять просто одно перемещение
+            if (speed > 3)
+            {
+                from.Peek().Top = from.Peek().Top - s_y;
+            }
+            else
+            {
+                for (int i = 0; i < Math.Abs(s_y) / speed; i++)
+                {
+                    from.Peek().Top = from.Peek().Top - speed;
+                    Thread.Sleep(time);
+                }
+            }
+                
+
+            int sgn = Math.Abs(s_x) / s_x; // чтоб правильно вычислять в какую сторону двигать диск ( влево и враво)
+            for (int i = 0; i < Math.Abs(s_x)/speed; i++)
+            {
+                from.Peek().Left = from.Peek().Left + speed * sgn;
+                Thread.Sleep(time);
+            }
+        // если скорость от 3 и более вертикальная анимация будет представлять просто одно перемещение
+            if (speed > 3)
+                {
+                    from.Peek().Top = from.Peek().Top + s_y2;
+                }
+            else
+            {
+                for (int i = 0; i < Math.Abs(s_y2 / speed); i++)
+                {
+                    from.Peek().Top = from.Peek().Top + speed;
+                    Thread.Sleep(time);
+                }
+            }
+            
+
+
+        }
+        // главная фунция решения 
+        private void solve()
+        {
+            int n = Decimal.ToInt32(numericUpDown1.Value);// количество дисков
+            // "Цикличное решение"
+            /*  Обозначим через «1 - 2» такое действие: переложить диск или с 1 - го штыря на 2 - й,
+                  или со 2 - го на 1 - й, в зависимости от того, где он меньше. Тогда,
+                  чтобы решить головоломку с чётным количеством дисков,
+                  надо многократно повторять действия: 1 - 2, 1 - 3, 2 - 3.
+                  Если число дисков нечётно — 1 - 3, 1 - 2, 2 - 3.*/
+            //"Цикличное решение"
+            if (n%2==0)
+                {
+                    while (tower_3_picturebox.Count != n )
+                    {
+                    // 1 - 2
+                        if (can_add(tower_1_picturebox, tower_2_picturebox))
+                        {
+                            move(tower_1_picturebox, tower_2_picturebox, pic_tower_1, pic_tower_2);// 1 -> 2
+
+                        }
+                        else if (tower_3_picturebox.Count != n)
+                    {
+                            move(tower_2_picturebox, tower_1_picturebox, pic_tower_2, pic_tower_1);//2 -> 1
+                        }
+                        // 1- 3
+                        if  (can_add(tower_1_picturebox, tower_3_picturebox) && tower_3_picturebox.Count != n)
+                        {
+                            move(tower_1_picturebox, tower_3_picturebox, pic_tower_1, pic_tower_3);// 1 -> 3
+                        }
+                        else if (tower_3_picturebox.Count != n)
+                    {
+                            move(tower_3_picturebox, tower_1_picturebox, pic_tower_3, pic_tower_1);//3 -> 1
+                        }
+                        // 2 - 3
+                        if (can_add(tower_2_picturebox, tower_3_picturebox) && tower_3_picturebox.Count != n)
+                    {
+                            move(tower_2_picturebox, tower_3_picturebox, pic_tower_2, pic_tower_3);// 2 -> 3
+                        }
+                        else if (tower_3_picturebox.Count != n)
+                    {
+                            move(tower_3_picturebox, tower_2_picturebox, pic_tower_3, pic_tower_2);//3 -> 2
+                        }
+                    }
+                }
+               else
+                {
+                    while (tower_3_picturebox.Count != n )
+                    {
+
+                    // 1- 3
+                    if (can_add(tower_1_picturebox, tower_3_picturebox))
+                        {
+                            move(tower_1_picturebox, tower_3_picturebox, pic_tower_1, pic_tower_3);// 1 -> 3
+                        }
+                        else if (tower_3_picturebox.Count != n)
+                    {
+                            move(tower_3_picturebox, tower_1_picturebox, pic_tower_3, pic_tower_1);//3 -> 1
+                        }
+                        // 1 - 2
+                        if (can_add(tower_1_picturebox, tower_2_picturebox) && tower_3_picturebox.Count != n)
+                        {
+                            move(tower_1_picturebox, tower_2_picturebox, pic_tower_1, pic_tower_2);// 1 -> 2
+                        }
+                        else if ( tower_3_picturebox.Count != n)
+                        {
+                            move(tower_2_picturebox, tower_1_picturebox, pic_tower_2, pic_tower_1);//2 -> 1
+                        }
+                        // 2 - 3
+                        if (can_add(tower_2_picturebox, tower_3_picturebox) && tower_3_picturebox.Count != n)
+                        {
+                            move(tower_2_picturebox, tower_3_picturebox, pic_tower_2, pic_tower_3);// 2 -> 3
+                        }
+                        else if (tower_3_picturebox.Count != n)
+                    {
+                            move(tower_3_picturebox, tower_2_picturebox, pic_tower_3, pic_tower_2);//3 -> 2
+                        }
+                    }
+
+                }
+
+
+        }
+
+        
+
+        private void create_disks(int n) // создание дисков ( чисто графика )
+        {
+            
+            int weight = 350 / n;// длина дисков ( подбирал их сам , именно подбирал...)
+            int hight = 388 / n;// ширина дисков
+
+
+       
+            for (int i = n; i >=1; i--)
+            {
+                Random random = new Random();
+                CustomPictureBox pic = new CustomPictureBox();
+                pic.BorderColorFirst = Color.FromArgb(random.Next(255) * i % 255, random.Next(255) * i % 255, random.Next(255) * i % 255);// рандомные цвета самих ДИСКОВ
+                pic.BackColor = Color.Black;// это цвет РАМКИ !
+                pic.Location = new Point(179 - (weight * (i)) / 2, 596 - (n - (i - 1)) * hight);// местоположение дисков
+                pic.Size = new Size(weight * i, hight);//размеры
+                this.Controls.Add(pic);// само добавление на форму 
+                tower_1_picturebox.Push(pic);// закидываем в стэк, чтобы потом с ними работать
+                pic.BringToFront();// на передний план диски ( иначе перекрывает башнями )
+            }
+        }
+
+        
+        
+
+        private void button_solve_Click(object sender, EventArgs e) // нажимаем на кнопку solve  и все начинает двигаться
+        {
+            
+            solve();// запуск решения
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) // при изменение значения нумерика...3
+        {
+            // удаление предыдущих дисков ( чтобы изчесли из формы )
+            foreach (PictureBox pic in tower_1_picturebox) 
+                pic.Dispose();
+            foreach (PictureBox pic in tower_2_picturebox) 
+                pic.Dispose();
+            foreach (PictureBox pic in tower_3_picturebox) 
+                pic.Dispose();
+            // удаление предыдущих дисков
+
+            // также удаление из стеков
+            tower_1_picturebox.Clear();
+            tower_2_picturebox.Clear();
+            tower_3_picturebox.Clear();
+            // также удаление из стеков
+
+            // получение числа с нумерик... и запуск функции создании дисков
+            int value = Decimal.ToInt32(numericUpDown1.Value);
+            create_disks(value);
+        }
+ 
+    }
+
+}
+
